0a1a20d1adb2eaf28dd5f4a4a56006fd4bda81c5
yercko12 <talamilla.yercko@gmail.com>
2025-07-11 02:42:19 -0400

    Se agrega tabla de candidatos aceptados en tablecandidate

--- src/assets/Components/TableCandidates.jsx
+++ src/assets/Components/TableCandidates.jsx
@@ -19,7 +19,7 @@ const TableCandidates = () => {
                             <th>Nombre de la Mascota</th>
                             <th>por que desea adoptar</th>
                             <th>Convivencia</th>
-                            
+
 
                         </tr>
                     </thead>
@@ -30,7 +30,7 @@ const TableCandidates = () => {
                             <td>pepe@rodriguez.com</td>
                             <td>30</td>
                             <td>+56912345678</td>
-                            <td>Calle Falsa 123</td>
+                            <td>P. Sherman, Calle Wallaby 42</td>
                             <td>Casa</td>
                             <td>Sí</td>
                             <td>Max</td>
@@ -39,9 +39,54 @@ const TableCandidates = () => {
                             <button className='confirmedForm'>✅</button>
                             <button className='deleteForm'>❌</button>
                         </tr>
-                        
+
                     </tbody>
                 </table>
+            </div>
+
+            <div>
+                <div className="tableContainer1">
+                    <h3>Canditatos aceptados</h3>
+                    <table className='tableConfirmed'>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Correo</th>
+                                <th>Edad</th>
+                                <th>Celular</th>
+                                <th>Domicilio</th>
+                                <th>Tipo de Vivienda</th>
+                                <th>Permite Mascotas</th>
+                                <th>Nombre de la Mascota</th>
+                                <th>por que desea adoptar</th>
+                                <th>Convivencia</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>pepe</td>
+                                <td>rodriguez</td>
+                                <td>pepe@rodriguez.com</td>
+                                <td>30</td>
+                                <td>+56912345678</td>
+                                <td>P. Sherman, Calle Wallaby 42, Sidney</td>
+                                <td>Casa</td>
+                                <td>Sí</td>
+                                <td>Max</td>
+                                <td>Terapia</td>
+                                <td>Solo</td>
+                                <button className='callBtn'>📞</button>
+                                
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+
             </div>
 
         </>
