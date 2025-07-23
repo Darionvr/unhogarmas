e2b44c1ce519825dc5f40200089c9cab444c0019
yercko12 <talamilla.yercko@gmail.com>
2025-07-22 22:45:26 -0400

    Petprofile listo

--- src/assets/Views/PetProfile.jsx
+++ src/assets/Views/PetProfile.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
 import { Link } from 'react-router'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const PetProfile = () => {
+  const { id } = useParams();
+  const [animal, setAnimal] = useState(null);
+
+  useEffect(() => {
+    const fetchAnimal = async () => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/pets/${id}`);
+        const data = await res.json();
+        setAnimal({
+          nombre: data.name,
+          especie: data.specie,
+          tamaño: data.weight > 10 ? 'grande' : data.weight > 5 ? 'medianos' : 'enano',
+          edad: data.age,
+          sexo: data.gender,
+          peso: data.weight,
+          imagen: data.photo,
+          chip: data.chip,
+          descripcion: data.description
+        });
+      } catch (err) {
+        console.error('Error al obtener datos del animal:', err);
+      }
+    };
+
+    fetchAnimal();
+  }, [id]);
+
+  if (!animal) return <p>Cargando información de la mascota...</p>;
+
   return (
     <main className='pet-main'>
 
@@ -9,13 +39,13 @@ const PetProfile = () => {
         <img src="imgs\Pet-photo1.png" alt="" />
       </div>
       <div className="pet-info">
-        <h1> Nombre</h1>
-        <p>Raza: <span>Raza</span> </p>
-        <p>Edad: <span>00 Años</span></p>
-        <p>Peso: <span>00Kg</span></p>
-        <p>Sexo: <span>MF</span></p>
-        <p>Chip: <span>SI/NO</span></p>
-        <p>(Descripción) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quam quasi nobis quae corporis, cum temporibus eligendi sunt corrupti fugiat quo similique dolorum aliquid saepe odio facilis modi incidunt porro!</p>
+        <h1> {animal.nombre}</h1>
+        <p>Especie: <span>{animal.especie}</span> </p>
+        <p>Edad: <span>{animal.edad}</span></p>
+        <p>Peso: <span>{animal.peso} </span></p>
+        <p>Sexo: <span> {animal.sexo === 'male' ? 'Macho' : animal.sexo === 'female' ? 'Hembra' : 'Desconocido'}</span></p>
+        <p>Chip: <span>{animal.chip === true ? 'Sí tiene chip' : animal.chip === false ? 'No tiene chip' : 'Desconocido'}</span></p>
+        <p>Descripcion: {animal.descripcion}</p>
         <button className='melon-button'> <Link to={'/adoptionform'}>  Comenzar adopción</Link></button>
       </div>
     </main>
